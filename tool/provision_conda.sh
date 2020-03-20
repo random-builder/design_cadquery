d0f31155078590aa890d0d77e09f50fb2a3d8278
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2020-03-20 11:40:44 -0500

    tester

--- tool/provision_conda.sh
+++ tool/provision_conda.sh
@@ -18,14 +18,19 @@ echo $PATH
 
 eval "$(conda shell.bash hook)"
 
-#conda env remove -y -n cq_tester
-#conda create     -y -n cq_tester python=3.7
+# build enviro
+conda env remove -y -n cq_tester
+conda create     -y -n cq_tester python=3.7
 
 conda activate         cq_tester
 
+# build support
 conda install -y -c conda-forge \
 	conda-build \
 	conda-verify \
-	pythonocc-core=7.4.0
+
+# cadquery deps
+conda install -y -c dlr-sc \
+    pythonocc-core=7.4.0 \
 
 conda config --show
